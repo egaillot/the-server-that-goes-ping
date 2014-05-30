9fab03e967a95736fbfc66b89e4f370bb1566759
Renan Oliveira <renandemelo@gmail.com>
2014-05-30 16:16:36 +0200

    fixed host and port

--- source/mcping.js
+++ source/mcping.js
@@ -2,7 +2,7 @@ var http = require("http");
 
 exports.createServer = function(pinger){
   return http.createServer(function(request, response){
-    	pinger.ping("bla",333,function(num_players){
+    	pinger.ping("some.minecraft.server",9999,function(num_players){
 		response.end("some.minecraft.server: " + num_players + " players connected.");
 	});
   });
