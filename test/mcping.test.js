427c4721b51b0ead777a81e3873a9221a4c4d184
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2014-05-30 15:10:08 +0200

    We want the server to respond something

--- test/mcping.test.js
+++ test/mcping.test.js
@@ -1,12 +1,18 @@
-var expect = require("expect.js");
-var mcPing = require("../source/mcping");
+var expect = require("expect.js"),
+    request = require("request"),
+    mcPing = require("../source/mcping");
 
 describe("A Minecraft ping server", function () {
   it("tells how many players are connected", function (done) {
     var ourPingServer = mcPing.createServer();
 
     ourPingServer.listen(1789, function () {
-      done();
+      request("http://localhost:1789/some.minecraft.server/9999",
+              function (err, _, body) {
+                if(err) done(err);
+                expect(body).to.equal("some.minecraft.server: 42 players connected.");
+                done();
+              });
     });
   });
 });
