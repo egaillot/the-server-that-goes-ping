9e6b183202c71c095e4f35c4c0d17d76757537e0
Renan Oliveira <renandemelo@gmail.com>
2014-05-30 14:26:11 +0200

    Fixing test

--- test/mcping.test.js
+++ test/mcping.test.js
@@ -2,6 +2,6 @@ var expect = require("expect.js");
 
 describe("A Minecraft ping server", function () {
   it("fails", function () {
-    expect(2+2).to.eql(3);
+    expect(2+2).to.eql(4);
   });
 });
