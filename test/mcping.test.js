a3d8d2e1e2a8b10c609d50dfb113a922689f7552
Renan Oliveira <renandemelo@gmail.com>
2014-05-30 16:39:47 +0200

    addedmaim method

--- test/mcping.test.js
+++ test/mcping.test.js
@@ -3,22 +3,33 @@ var expect = require("expect.js"),
     mcPing = require("../source/mcping");
 
 describe("A Minecraft ping server", function () {
-  it("tells how many players are connected", function (done) {
+  it("a ping server that can actually ping...", function (done) {
     var pinger = {
           ping: function (host, port, callback) {
             expect(host).to.equal("some.minecraft.server");
             expect(port).to.equal(9999);
-            callback(42);
             done();
           }
         },
         ourPingServer = mcPing.createServer(pinger);
     ourPingServer.listen(1789, function () {
-      request("http://localhost:1789/some.minecraft.server/9999",
+      request("http://localhost:1789/some.minecraft.server/9999");
+    });
+  });
+  it("tells how many players are connected", function (done) {
+    var pinger = {
+          ping: function (host, port, callback) {
+            callback(45);
+          }
+        },
+        ourPingServer = mcPing.createServer(pinger);
+    ourPingServer.listen(1790, function () {
+      request("http://localhost:1790/some.minecraft.server/9999",
         function (err, _, body) {
           if(err) done(err);
-            expect(body).to.equal("some.minecraft.server: 42 players connected.");
+            expect(body).to.equal("some.minecraft.server: 45 players connected.");
           });
+	done();
       });
     });
 });
