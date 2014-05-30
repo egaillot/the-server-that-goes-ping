294fdb245bbd50d65b31679af44d8dd8140fa273
Demo User <fake@fake.com>
2014-05-30 16:17:34 +0200

    Removed harcoding

--- source/mcping.js
+++ source/mcping.js
@@ -4,7 +4,7 @@ exports.createServer = function(pinger){
   return http.createServer(function(request, response){
       var url = request.url.split("/");
     	pinger.ping(url[1], parseInt(url[2]), function(num_players){
-		response.end("some.minecraft.server: " + num_players + " players connected.");
+		response.end(url[1] + ": " + num_players + " players connected.");
 	});
   });
 };
