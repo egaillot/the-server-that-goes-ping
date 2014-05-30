f13daa8e44acac6f52bb16cf68693147e34297b5
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2014-05-30 15:52:24 +0200

    Need to go through the pinger

--- test/mcping.test.js
+++ test/mcping.test.js
@@ -4,14 +4,19 @@ var expect = require("expect.js"),
 
 describe("A Minecraft ping server", function () {
   it("tells how many players are connected", function (done) {
-    var ourPingServer = mcPing.createServer();
+    var pinger = {
+          ping: function (host, port, callback) {
+            callback(42);
+            done();
+          }
+        },
+        ourPingServer = mcPing.createServer(pinger);
 
     ourPingServer.listen(1789, function () {
       request("http://localhost:1789/some.minecraft.server/9999",
         function (err, _, body) {
           if(err) done(err);
             expect(body).to.equal("some.minecraft.server: 42 players connected.");
-            done();
           });
       });
     });
