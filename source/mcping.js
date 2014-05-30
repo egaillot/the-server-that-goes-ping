612d8aacd92ae4f9c12c38bba8e41041d4d2da96
Demo User <fake@fake.com>
2014-05-30 16:15:45 +0200

    Fix host and port

--- source/mcping.js
+++ source/mcping.js
@@ -2,7 +2,8 @@ var http = require("http");
 
 exports.createServer = function(pinger){
   return http.createServer(function(request, response){
-    	pinger.ping("bla",333,function(num_players){
+      var url = request.url.split("/");
+    	pinger.ping(url[1], parseInt(url[2]), function(num_players){
 		response.end("some.minecraft.server: " + num_players + " players connected.");
 	});
   });
